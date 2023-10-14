0d57b56cfc87fadfc20b888460025022ff199b6e
unknown <adnankhannad@gmail.com>
2023-10-14 23:22:29 +0500

    Add expense object to the expenses array

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -11,32 +11,27 @@ const Expenses = (props) => {
     console.log("Expenses.js");
     setFilteredYear(selectedYear);
   };
+
+  /*
+    ! Controlled and Uncontrolled Components
+    * Controlled Component: Whenever you use 2-way binding you are controlling a component
+
+  */
+  // Statefull and stateless components
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangerHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
